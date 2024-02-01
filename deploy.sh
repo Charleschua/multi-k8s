eb3d93fd32cba38fdac28c77b70087b3fcacf9c3
Charleschua <charleschua19@gmail.com>
2024-02-01 22:42:59 +0800

    aded deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t charles19/multi-client-k8s:latest -t charles19/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t charles19/multi-server-k8s:latest -t charles19/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
-docker build -t charles19/multi-worker-k8s:latest -t charles19/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t charles19/multi-client:latest -t charles19/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t charles19/multi-server:latest -t charles19/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t charles19/multi-worker:latest -t charles19/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push charles19/multi-client-k8s:latest
-docker push charles19/multi-server-k8s:latest
-docker push charles19/multi-worker-k8s:latest
+docker push charles19/multi-client:latest
+docker push charles19/multi-server:latest
+docker push charles19/multi-worker:latest
 
-docker push charles19/multi-client-k8s:$SHA
-docker push charles19/multi-server-k8s:$SHA
-docker push charles19/multi-worker-k8s:$SHA
+docker push charles19/multi-client:$SHA
+docker push charles19/multi-server:$SHA
+docker push charles19/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=charles19/multi-server-k8s:$SHA
-kubectl set image deployments/client-deployment client=charles19/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=charles19/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=charles19/multi-server:$SHA
+kubectl set image deployments/client-deployment client=charles19/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=charles19/multi-worker:$SHA
